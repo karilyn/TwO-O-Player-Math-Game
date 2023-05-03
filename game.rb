bb5d807ee4954fd9d3e5ae16ce160b2c4c519d14
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-05-03 13:50:13 -0700

    Removed all I/O logic

--- game.rb
+++ game.rb
@@ -3,31 +3,33 @@ require './question'
 
 class Game
 
-  attr_accessor :player1 :player2, :current_player
+  attr_accessor :player1, :player2, :current_player
 
   def initialize(player1, player2)
-    @player1 = Player.new(@name, 3)
-    @player2 = Player.new(@name, 3)
-    current_player = @player1
+    @player1 = Player.new(player1)
+    @player2 = Player.new(player2)
+    @current_player = @player1
   end
 
   def check_score
-    puts "#{@player1.name}: #{@player1.lives}/3 vs. #{@player2.name}: #{@player2.lives}/3"
-  end
-
-  def show_score
-    if @player1.lives > @player2.lives
-      puts @player1.lives
-    else
-      puts @player2.lives
+    "#{@player1.name}: #{@player1.lives}/3 vs. #{@player2.name}: #{@player2.lives}/3"
   end
 
   def winner
     if @player1.lives > @player2.lives
-      puts @player1.name
+      @player1
     else
-      puts @player2.name
+      @player2
+    end
   end
+
+  def finish_turn!(is_correct)
+    if !is_correct
+      @current_player.lose_life
+    end
+    switch_player
+  end
+
 
   def switch_player
     if @current_player == @player1
@@ -39,21 +41,13 @@ class Game
 
   def game_over?
     @player1.is_dead? || @player2.is_dead?
-    puts "#{winner} wins with a score of #{show_score}"
-    "----- GAME OVER -----"
   end
 
-
-  def play_game
-    # loop through until game ends
-    while !game_over?
-      Question.new.ask_question(@current_player)
-      check_score
-      switch_player
-      puts "----- NEW TURN -----"
-    end
-    end_game
-
+  def result
+    {
+      :winner => winner.name,
+      :score => winner.lives
+    }
   end
-end
 
+ end
